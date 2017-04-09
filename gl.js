566edda5f26a2b8bd8b6c0c7d98374621550fb06
tknott95 <tknott95@hotmail.com>
2017-04-09 17:29:25 -0600

    Cleaner Shaders

--- gl.js
+++ gl.js
@@ -1,5 +1,5 @@
-var gl,
-    shaderProgram;
+var gl;
+var shaderProgram;
 
 initGL();
 createShaders();
@@ -15,28 +15,12 @@ function initGL() {
 }
 
 function createShaders() {
-    var vs = "";
-    vs += "attribute vec4 coords;";
-    vs += "attribute float pointSize;";
-    vs += "void main(void) {";
-    vs += "  gl_Position = coords;";
-    vs += "  gl_PointSize = pointSize;";
-    vs += "}";
 
-    var vertexShader = gl.createShader(gl.VERTEX_SHADER);
-    gl.shaderSource(vertexShader, vs);
-    gl.compileShader(vertexShader);
 
-    var fs = "";
-    fs += "precision mediump float;";
-    fs += "uniform vec4 color;";
-    fs += "void main(void) {";
-    fs += "  gl_FragColor = color;";
-    fs += "}";
+    var vertexShader = getShader(gl, "shader-vs")
 
-    var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-    gl.shaderSource(fragmentShader, fs);
-    gl.compileShader(fragmentShader);
+    var fragmentShader = getShader(gl, "shader-fs");
+
 
     shaderProgram = gl.createProgram();
     gl.attachShader(shaderProgram, vertexShader);
@@ -66,20 +50,54 @@ function createVertices() {
     gl.vertexAttrib1f(pointSize, 100);
 
     var color = gl.getUniformLocation(shaderProgram, "color");
-    gl.uniform4f(color, 1, 0, 1, 1);
+    gl.uniform4f(color, 1, 1, 0, 0);
 }
 
 function draw() {
     gl.clear(gl.COLOR_BUFFER_BIT);
-    gl.drawArrays(gl.POINTS, 0, 3);
+    gl.drawArrays(gl.TRIANGLES, 0, 3);
 }
 
+/*
+ * https://developer.mozilla.org/en-US/docs/Web/API/WebGL_API/Tutorial/Adding_2D_content_to_a_WebGL_context
+ */
+function getShader(gl, id) {
+    var shaderScript, theSource, currentChild, shader;
 
+    shaderScript = document.getElementById(id);
 
+    if (!shaderScript) {
+        return null;
+    }
 
+    theSource = "";
+    currentChild = shaderScript.firstChild;
 
+    while (currentChild) {
+        if (currentChild.nodeType == currentChild.TEXT_NODE) {
+            theSource += currentChild.textContent;
+        }
 
+        currentChild = currentChild.nextSibling;
+    }
+    if (shaderScript.type == "x-shader/x-fragment") {
+        shader = gl.createShader(gl.FRAGMENT_SHADER);
+    } else if (shaderScript.type == "x-shader/x-vertex") {
+        shader = gl.createShader(gl.VERTEX_SHADER);
+    } else {
+        // Unknown shader type
+        return null;
+    }
+    gl.shaderSource(shader, theSource);
 
+// Compile the shader program
+    gl.compileShader(shader);
 
+// See if it compiled successfully
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        alert("An error occurred compiling the shaders: " + gl.getShaderInfoLog(shader));
+        return null;
+    }
 
-
+    return shader;
+}
