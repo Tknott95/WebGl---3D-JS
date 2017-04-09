941b64d9a6c48f51f59b00fbb96597ebb020a22c
tknott95 <tknott95@hotmail.com>
2017-04-09 17:18:48 -0600

    3 Points drawn now

--- gl.js
+++ gl.js
@@ -1,5 +1,5 @@
-var gl;
-var shaderProgram;
+var gl,
+    shaderProgram;
 
 initGL();
 createShaders();
@@ -8,6 +8,7 @@ draw();
 
 function initGL() {
     var canvas = document.getElementById("canvas");
+    console.log(canvas);
     gl = canvas.getContext("webgl");
     gl.viewport(0, 0, canvas.width, canvas.height);
     gl.clearColor(0, 0, 0, 1);
@@ -16,9 +17,10 @@ function initGL() {
 function createShaders() {
     var vs = "";
     vs += "attribute vec4 coords;";
+    vs += "attribute float pointSize;";
     vs += "void main(void) {";
-    vs += " gl_Position = coords;";
-    vs += " gl_PointSize = 100.0;";
+    vs += "  gl_Position = coords;";
+    vs += "  gl_PointSize = pointSize;";
     vs += "}";
 
     var vertexShader = gl.createShader(gl.VERTEX_SHADER);
@@ -26,8 +28,10 @@ function createShaders() {
     gl.compileShader(vertexShader);
 
     var fs = "";
+    fs += "precision mediump float;";
+    fs += "uniform vec4 color;";
     fs += "void main(void) {";
-    fs += " gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);";
+    fs += "  gl_FragColor = color;";
     fs += "}";
 
     var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
@@ -39,16 +43,43 @@ function createShaders() {
     gl.attachShader(shaderProgram, fragmentShader);
     gl.linkProgram(shaderProgram);
     gl.useProgram(shaderProgram);
-
-
 }
 
 function createVertices() {
+    vertices = [
+        -0.9, -0.9, 0.0,
+        0.9, -0.9, 0.0,
+        0.0,  0.9, 0.0
+    ];
+
+    var buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+
     var coords = gl.getAttribLocation(shaderProgram, "coords");
-    gl.vertexAttrib3f(coords, 0, 0, 0);
+    //gl.vertexAttrib3f(coords, 0.5, 0.5, 0);
+    gl.vertexAttribPointer(coords, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(coords);
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+    var pointSize = gl.getAttribLocation(shaderProgram, "pointSize");
+    gl.vertexAttrib1f(pointSize, 100);
+
+    var color = gl.getUniformLocation(shaderProgram, "color");
+    gl.uniform4f(color, 1, 0, 1, 1);
 }
 
 function draw() {
     gl.clear(gl.COLOR_BUFFER_BIT);
-    gl.drawArrays(gl.POINTS, 0, 1);
+    gl.drawArrays(gl.POINTS, 0, 3);
 }
+
+
+
+
+
+
+
+
+
+
